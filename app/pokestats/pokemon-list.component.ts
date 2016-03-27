49aef3e0758892e887d33993ca501c79898e6eed
desko27 <desko27@gmail.com>
2016-03-27 17:35:06 +0200

    allow scrolling pokemon results in search sidebar

--- app/pokestats/pokemon-list.component.ts
+++ app/pokestats/pokemon-list.component.ts
@@ -9,10 +9,14 @@ import {FirstCapitalLetter} from '../pipes/first-capital-letter.pipe';
     pipes: [LastUriSegment, FirstCapitalLetter],
     inputs: ['search'],
     template: `
-    <li *ngFor="#pokemon of pokemons" class="pokemon-item valign-content" data-id="{{ pokemon.resource_uri | lastUriSegment }}">
-        <span class="name valigned">{{ pokemon.name | firstCapitalLetter }}<div class="separator"></div></span>
-        <span class="icon valigned"><img src="assets/pokemon/icon/{{ pokemon.resource_uri | lastUriSegment }}.png"></span>
-    </li>
+    <div id="pokemon-list-container">
+    <ul id="pokemon-list">
+        <li *ngFor="#pokemon of pokemons" class="pokemon-item valign-content" data-id="{{ pokemon.resource_uri | lastUriSegment }}">
+            <span class="name valigned">{{ pokemon.name | firstCapitalLetter }}<div class="separator"></div></span>
+            <span class="icon valigned"><img src="assets/pokemon/icon/{{ pokemon.resource_uri | lastUriSegment }}.png"></span>
+        </li>
+    </ul>
+    </div>
     `
 })
 export class PokemonListComponent implements OnInit {
@@ -37,7 +41,6 @@ export class PokemonListComponent implements OnInit {
     }
 
     filterPokemons(pokemons) {
-        console.log(this.search);
         return pokemons.filter(function(item) {
             var id = new LastUriSegment().transform(item.resource_uri);
             return id <= 386 // 3rd generation
