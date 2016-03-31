3bf16cac166b8c15315532b0916c2d7016e141d9
desko27 <desko27@gmail.com>
2016-03-31 22:10:15 +0200

    less complicated loading bar

--- app/services/nanobar.service.ts
+++ app/services/nanobar.service.ts
@@ -5,7 +5,6 @@ export class NanobarService {
     
     private bar: any = null;
     private progress: number = 0;
-    private last_tictac_counter: number = 0;
     private finished: boolean = true;
 
     constructor() { this.bar = new Nanobar({ id: 'nanobar' }); }
@@ -16,38 +15,21 @@ export class NanobarService {
     public start() {
 
         this.finished = false;
-        this.progress = 0;
-        this.last_tictac_counter = 0;
+        this.progress = 15;
         increase(this);
 
         function increase(self: NanobarService) {
 
             if (!self.finished) {
 
-                var speed_rate = 1.75;
-                var max_value = 80;
+                var speed_rate = 5;
+                var max_value = 95;
                 var x = self.progress;
 
-                var absolute_max = 97;
-                var last_tictac_counter_max = 15;
-                var last_tictac_power = 0.65;
+                self.progress += speed_rate * (max_value - x) / max_value;
+                self.go(self.progress);
 
-                if (self.progress <= max_value)
-                    self.progress += speed_rate * (max_value - x) / max_value;
-                
-                if (self.progress > max_value - 10) {
-
-                    if (self.last_tictac_counter < last_tictac_counter_max)
-                        self.last_tictac_counter++;
-                    else {
-                        self.progress += last_tictac_power;
-                        self.last_tictac_counter = 0;
-                    }
-
-                }
-
-                self.go(Math.min(self.progress, absolute_max));
-                setTimeout(increase, 25, self);
+                setTimeout(increase, 500, self);
             }
 
         }
